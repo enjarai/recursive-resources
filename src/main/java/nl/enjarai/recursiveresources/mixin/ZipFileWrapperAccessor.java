1015fdfb3be47ef9e025ba02fba00edabdcb027b
ImBonana <eladtairo@gmail.com>
2025-07-02 01:18:20 +0300

    Fix compiler warnings

--- src/main/java/nl/enjarai/recursiveresources/mixin/ZipFileWrapperAccessor.java
+++ src/main/java/nl/enjarai/recursiveresources/mixin/ZipFileWrapperAccessor.java
@@ -1,11 +1,12 @@
 package nl.enjarai.recursiveresources.mixin;
 
+import net.minecraft.resource.ZipResourcePack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import java.io.File;
 
-@Mixin(targets = "net/minecraft/resource/ZipResourcePack$ZipFileWrapper")
+@Mixin(ZipResourcePack.ZipFileWrapper.class)
 public interface ZipFileWrapperAccessor {
     @Accessor
     File getFile();
